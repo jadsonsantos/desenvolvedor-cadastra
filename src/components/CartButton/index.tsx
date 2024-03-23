305bd2d2e52e2f3098fbe771c5e9222fd71bf183
Jadson Santos <jadsondossantos5@gmail.com>
2024-03-23 17:46:21 -0300

    feat: create cart button component

---
+++ src/components/CartButton/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CartButton = () => {
+  return (
+    <div className="cart-button">
+      <img src="../../img/icons/bag.svg" />
+      <p className="cart-button__count">0</p>
+    </div>
+  )
+}
+
+export default CartButton
